88f5c0725911f8e93a36a54bf9d2a5d5fc12292c
Lucas Pereira Coutinho da Silva <lp172910@gmail.com>
2022-10-03 13:44:26 -0300

    Adicionando ícone e App bar

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -4,14 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +24,9 @@ public class MainActivity extends Activity {
         ListView ListaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
 
         ListaDeAlunos.setAdapter(new ArrayAdapter<>(this,
-                android.R.layout.simple_expandable_list_item_1, alunos));
+                android.R.layout.simple_list_item_1, alunos));
+
+        setTitle("Lista de alunos");
 
         /* Criando uma text view
         TextView aluno = new TextView(this);
